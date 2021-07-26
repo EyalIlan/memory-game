e4d4f51c88677e298c3fad1a0401a281bea1d9a9
EyalIlan <eyalilan8@gmail.com>
2021-07-26 21:28:16 +0300

    first files

---
+++ src/Components/Pages/Game/game.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import Axios from '../../Utils/axios'
+
+
+interface Cardobj  {
+    firstCard:number,
+    secondCard:number
+    numberOfErrors:number
+    Score:number
+}
+
+
+const game:React.FC<{name:string}> =(props) => {
+
+    const [data,SetData] = useState([])
+
+
+    let obj:Cardobj = {
+        firstCard: -1,
+        secondCard:-1,
+        numberOfErrors:0,
+        Score:0
+    }
+
+
+    useEffect(() =>{
+
+      const request = async () =>{
+
+        const responce = await Axios.get('/?page=1')
+        
+        console.log(responce);
+        
+        SetData(responce.data)
+
+
+      }      
+      request()
+    },[])
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default game
